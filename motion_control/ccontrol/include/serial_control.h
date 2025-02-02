fe49eb9369ac74d45008ef86d1990588fc262aca
chen.xu <nicolasxu93@163.com>
2025-02-02 16:50:52 +0800

    add ccontrol

--- motion_control/ccontrol/include/serial_control.h
+++ motion_control/ccontrol/include/serial_control.h
@@ -9,6 +9,8 @@
 #include <vector>
 #include <tuple>
 #include <libserial/SerialPort.h>
+#include <string.h>
+#include <algorithm>
 
 #define PI 3.1415926
 #define P_MIN -4 * PI
@@ -21,13 +23,58 @@
 #define KP_MAX 500.0
 #define KD_MIN 0.0
 #define KD_MAX 5.0
+#define TWO_BYTES_BITS 16
 
 namespace SerialController {
     
 using namespace std;
 using namespace LibSerial;
 
+// 通信类型
+enum class CmdModes {
+    GET_DEVICE_ID = 0,
+    MOTOR_CONTROL = 1,
+    MOTOR_FEEDBACK = 2,
+    MOTOR_ENABLE = 3,
+    MOTOR_STOP = 4,
+    SET_MECHANICAL_ZERO = 6,
+    SET_MOTOR_CAN_ID = 7,
+    PARAM_TABLE_WRITE = 8,
+    SINGLE_PARAM_READ = 17,
+    SINGLE_PARAM_WRITE = 18,
+    FAULT_FEEDBACK = 21
+};
 
+// 控制模式
+enum class RunModes {
+    CONTROL_MODE = 0, // 运控模式
+    POSITION_MODE = 1, // 位置模式
+    SPEED_MODE = 2, // 速度模式
+    CURRENT_MODE = 3 // 电流模式
+};
+
+struct Decode8BytesData {
+    string format;
+    uint8_t u8_res;
+    float f_res;
+    float pos;
+    float vel;
+    float torque;
+    float temperature_celsius;
+    // 重写输出<<，用于输出结构体内容
+    friend ostream& operator<<(ostream& os, const Decode8BytesData& data) {
+        os << "format: " << data.format ;
+        if (data.format == "ba") {
+            os << "pos: " << data.pos << " vel: " << data.vel << 
+                " torque: " << data.torque << 
+                " temperature: " << data.temperature_celsius;
+        } else if (data.format == "u8"){
+            os << "result is: " << data.u8_res;
+        } else {
+            os << "result is: " << data.f_res;
+        }
+    }
+};
 
 // 串口控制类接口
 class SerialControllerInterface {
@@ -50,6 +97,30 @@ public:
 private:
     // 进入AT模式   
     void enter_AT_mode_();
+    // 编码数据
+    std::vector<uint8_t> encode_data_(uint8_t cmd_mode, 
+                    const std::vector<uint8_t>& index = {}, 
+                    const std::string& format = "", 
+                    float value = 0.0, 
+                    float min = 0.0, 
+                    float max = 0.0, 
+                    const std::vector<uint8_t>& bit29 = {}); 
+
+    std::vector<uint8_t> encode_canid_(uint8_t cmd_mode);
+
+    std::vector<uint8_t> encode_8_bytes_data_(const std::vector<uint8_t>& index, 
+                                                const std::string& format, 
+                                                float value, 
+                                                float x_min, 
+                                                float x_max);
+    
+    // 解码数据
+    Decode8BytesData _parse_received_msg(const std::vector<uint8_t>& received_msg_data, 
+                                    const std::string& format = "f");
+
+    std::vector<uint8_t> decode_canid_(const std::vector<uint8_t>& ex_can_id);
+
+    Decode8BytesData _decode_8_bytes_data(const std::vector<uint8_t>& data, const std::string& format);
 
 private:
     SerialPort serial_port_;
@@ -64,6 +135,67 @@ private:
     map<string, tuple<int, string>> param_table_;
     map<string, tuple<int, string, float, float>> parameters;
 };
+
+
+
+uint32_t merge_big_endian(const std::vector<uint8_t>& bytes) {
+    if (bytes.size() > 4) {
+        std::cerr << "Input vector has more than 4 bytes, cannot fit into uint32_t." << std::endl;
+        return 0;
+    }
+
+    uint32_t result = 0;
+    // 大端法合并
+    for (size_t i = 0; i < bytes.size(); ++i) {
+        result |= static_cast<uint32_t>(bytes[i]) << ((3 - i) * 8);
+    }
+}
+
+// 将 uint32_t 按照大端法拆分成 std::vector<uint8_t>
+std::vector<uint8_t> split_big_endian(uint32_t value) {
+    std::vector<uint8_t> bytes(4);
+    for (int i = 0; i < 4; ++i) {
+        // 右移相应位数并取最低 8 位
+        bytes[i] = static_cast<uint8_t>((value >> ((3 - i) * 8)) & 0xFF);
+    }
+    return bytes;
+}
+
+float _uint_to_float(uint16_t x, float x_min, float x_max, uint16_t bits) {
+        /**
+         * 将无符号整数转换为浮点数。
+         *
+         * 参数:
+         * x: 输入的无符号整数。
+         * x_min: 可接受的最小浮点数。
+         * x_max: 可接受的最大浮点数。
+         * bits: 输入无符号整数的位数。
+         *
+         * 返回:
+         * 转换后的浮点数。
+         */
+        uint16_t span = (1 << bits) - 1;
+        float offset = x_max - x_min;
+        x = std::max(std::min(x, span), (uint16_t)0);  // Clamp x to the range [0, span]
+        return offset * x / span + x_min;
+    }
+
+    float _linear_mapping(float value, float value_min, float value_max, float target_min = 0.0, float target_max = 65535.0) {
+        /**
+         * 对输入值进行线性映射。
+         *
+         * 参数:
+         * value: 输入值。
+         * value_min: 输入值的最小界限。
+         * value_max: 输入值的最大界限。
+         * target_min: 输出值的最小界限。
+         * target_max: 输出值的最大界限。
+         *
+         * 返回:
+         * 映射后的值。
+         */
+        return static_cast<int>((value - value_min) / (value_max - value_min) * (target_max - target_min) + target_min);
+    }
 
 
 } // namespace SerialController
--- motion_control/ccontrol/src/serial_control.cpp
+++ motion_control/ccontrol/src/serial_control.cpp
@@ -32,10 +32,152 @@ SerialControllerInterface::SerialControllerInterface(const int& motor_id,
 }
 
 void SerialControllerInterface::enter_AT_mode_() {
-    int hex = 0x41542b41540d0a;
-    std::stringstream ss;
-    ss << std::hex << hex;
-    serial_port_.Write(ss.str()) ;
+    string str;
+    str.push_back(0x41);
+    str.push_back(0x54);
+    str.push_back(0x2b);
+    str.push_back(0x41);
+    str.push_back(0x54);
+    str.push_back(0x0d);
+    str.push_back(0x0a);
+    serial_port_.Write(str) ;
+    // std::cout << "Enter AT mode" << std::endl;
+}
+
+std::vector<uint8_t> SerialControllerInterface::encode_data_(uint8_t cmd_mode, 
+                                            const std::vector<uint8_t>& index = {}, 
+                                            const std::string& format = "", 
+                                            float value = 0.0, 
+                                            float min = 0.0, 
+                                            float max = 0.0, 
+                                            const std::vector<uint8_t>& bit29 = {}){
+    std::vector<uint8_t> data_frame(17);
+    // 帧头两个字节
+    data_frame[0] = 0x41;
+    data_frame[1] = 0x54;
+    if (bit29.empty()) {
+        // 计算4个字节长度的ex_can_id
+        std::vector<uint8_t> ex_can_id = encode_canid_(cmd_mode);
+        // 将ex_can_id转换为字节，并填充到字节数组中
+        std::copy(ex_can_id.begin(), ex_can_id.end(), data_frame.begin() + 2);
+    } else {
+        std::copy(bit29.begin(), bit29.end(), data_frame.begin() + 2);
+    }
+    // 数据位个数放到第六位上
+    data_frame[6] = 0x08;
+    // 组合数据帧
+    if (index.empty() && value != 0.0) {
+        std::copy(reinterpret_cast<const uint8_t*>(&value), reinterpret_cast<const uint8_t*>(&value) + sizeof(value), data_frame.begin() + 7);
+    } else {
+        std::vector<uint8_t> data = encode_8_bytes_data_(index, format, value, min, max);
+        std::copy(data.begin(), data.end(), data_frame.begin() + 7);
+    }
+    // 帧尾两个字节
+    data_frame[15] = 0x0d;
+    data_frame[16] = 0x0a;
+    return data_frame;
+}
+
+std::vector<uint8_t> SerialControllerInterface::encode_canid_(uint8_t cmd_mode) {
+    // 0. 声明一个字节数组，长度为4
+    std::vector<uint8_t> canid(4);
+    // 1. 将通讯模式cmd_mode转换为字节
+    canid[0] = cmd_mode;
+    // 2. 填充0x00到第二个字节
+    canid[1] = 0x00;
+    // 3. 将主机canid转换为字节，并填充到字节数组中
+    canid[2] = main_can_id_;
+    // 4. 将电机canid转换为字节，并填充到字节数组中
+    canid[3] = motor_id_;
+    // 5. 用16进制形式打印组合后的字节数组
+    // print("组合扩展帧canid后的字节数组:", canid.hex())
+    // 6. 将字节数组左移三位并把第二位置为1
+    uint32_t canid_int = merge_big_endian(canid);
+    canid_int = canid_int << 3;
+    canid_int = canid_int | 0x04;
+    // print("最终的扩展帧canid字节数组:", hex(canid))
+    return split_big_endian(canid_int);
+}
+
+
+std::vector<uint8_t> encode_8_bytes_data_(const std::vector<uint8_t>& index, 
+                                                const std::string& format, 
+                                                float value, 
+                                                float x_min, 
+                                                float x_max){
+     std::vector<uint8_t> data_frame(8);
+        // 将index转换为字节，并填充到字节数组中,
+        if (!index.empty()) {
+            std::copy(index.begin(), index.end(), data_frame.begin());
+        }
+        data_frame[2] = 0x00;
+        data_frame[3] = 0x00;
+        // 将value转换为字节，并填充到字节数组中
+        if (value != 0.0 && format != "ba") {
+            value = std::max(std::min(value, x_max), x_min);
+            if (format == "u8") {
+                data_frame[4] = static_cast<uint8_t>(value);
+            } else if (format == "f") {
+                memcpy(data_frame.data() + 4, &value, sizeof(value));
+            }
+        } else if (value != 0.0 && format == "ba") {
+            std::copy(reinterpret_cast<const uint8_t*>(&value), reinterpret_cast<const uint8_t*>(&value) + sizeof(value), data_frame.begin() + 4);
+        }
+        return data_frame;
+}
+
+Decode8BytesData SerialControllerInterface::_parse_received_msg(const std::vector<uint8_t>& received_msg_data, const std::string& format = "f") {
+        
+        std::vector<uint8_t> ex_can_id_msg(received_msg_data.begin()+2, received_msg_data.begin()+6);
+        std::vector<uint8_t> ex_can_id = decode_canid_(ex_can_id_msg);
+        std::cout << "ex_can_id is: " << merge_big_endian(ex_can_id) << std::endl;
+        // 数据帧
+        std::vector<uint8_t> data(received_msg_data.begin()+7, received_msg_data.begin()+15);
+        Decode8BytesData result = _decode_8_bytes_data(data, format);
+        // if (format == "ba") {
+        //     std::cout << "pos: " << result.pos << std::endl;
+        //     std::cout << "vel: " << result.vel << std::endl;
+        //     std::cout << "torque: " << result.torque << std::endl;
+        //     std::cout << "temperature_celsius: " << result.temperature_celsius << std::endl;
+        // } else if (format == "u8"){
+        //     std::cout << "result is: " << result.u8_res << std::endl;
+        // } else {
+        //     std::cout << "result is: " << result.f_res << std::endl;
+        // }
+        return result;
+    }
+
+std::vector<uint8_t> SerialControllerInterface::decode_canid_(const std::vector<uint8_t>& ex_can_id) {
+        // 判断ex_can_id的type是否是bytearray
+        uint32_t canid_int = merge_big_endian(ex_can_id);
+        canid_int = canid_int >> 3;
+        // print("解析后的canid字节数组: ", hex(canid))
+        // 将int转换为bytearray
+        return split_big_endian(canid_int);
+}
+
+Decode8BytesData SerialControllerInterface::_decode_8_bytes_data(const std::vector<uint8_t>& data, const std::string& format) {
+    Decode8BytesData res;
+    res.format = format;
+    if (format == "u8") {
+        // 将字节转位uint8
+        res.u8_res =  static_cast<uint8_t>(data[4]);
+    } else if (format == "f") {
+        // 将字节数组转换为浮点数
+        memcpy(&res.f_res, data.data(), sizeof(res.f_res));
+    
+    } else if (format == "ba") {
+        // 将前两个字节[0-65535]映射到[-4pi , 4pi]
+        res.pos = _uint_to_float((data[0] << 8) + data[1], P_MIN, P_MAX, TWO_BYTES_BITS);
+        // 将第3和第4字节[0-65535]映射到[-30.0 , 30.0]
+        res.vel = _uint_to_float((data[2] << 8) + data[3], V_MIN, V_MAX, TWO_BYTES_BITS);
+        // 将第5和第6字节[0-65535]映射到[-12.0 , 12.0]
+        res.torque = _uint_to_float((data[4] << 8) + data[5], T_MIN, T_MAX, TWO_BYTES_BITS);
+        // 将第7和第8字节[0-65535]除以10.0得到温度值
+        res.temperature_celsius = static_cast<float>((data[6] << 8) + data[7]) / 10.0;
+        
+    }
+    return res;
 }
 
 } // namespace SerialController
