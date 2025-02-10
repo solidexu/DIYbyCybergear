5a5c0387eedc7a690ed4dabf992e3ed649061b24
chen.xu <nicolasxu93@163.com>
2025-02-10 21:02:36 +0800

    4 motor test

--- motion_control/serial_control/test/serial_control_test.cpp
+++ motion_control/serial_control/test/serial_control_test.cpp
@@ -18,32 +18,40 @@ TEST(SerialControlTest, POSITION_MODE) {
     sleep(1);
     using namespace SerialController;
     std::shared_ptr<SerialPort> serial = std::make_shared<SerialPort>();
+    SerialControllerInterface *motor1 = new SerialControllerInterface(serial, 1);
     SerialControllerInterface *motor2 = new SerialControllerInterface(serial, 2);
     SerialControllerInterface *motor3 = new SerialControllerInterface(serial, 3);
     SerialControllerInterface *motor4 = new SerialControllerInterface(serial, 4);
     motor4->set_motor_0position(); // set motor 0 position
     motor3->set_motor_0position(); // set motor 0 position
     motor2->set_motor_0position(); // set motor 0 position
+    motor1->set_motor_0position(); // set motor 0 position
     sleep(1); // sleep 1s
     motor4->enable_motor();
     motor3->enable_motor();
     motor2->enable_motor();
+    motor1->enable_motor();
     sleep(1); // sleep 1s
     motor4->set_run_mode(RunModes::POSITION_MODE);
     motor3->set_run_mode(RunModes::POSITION_MODE);
     motor2->set_run_mode(RunModes::POSITION_MODE);
+    motor1->set_run_mode(RunModes::POSITION_MODE);
     sleep(1); // sleep 1s
     motor2->set_motor_position_control(2.0, -0.8);
     motor4->set_motor_position_control(1.0, 3.14);
     motor3->set_motor_position_control(1.0, 0.5);
-    sleep(1); // sleep 1s
+    motor1->set_motor_position_control(2.0, -0.5);
+    sleep(2); // sleep 1s
+    
     motor4->set_motor_position_control(3.0, -3.14);
-    motor3->set_motor_position_control(1.0, 0.1);
+    motor1->set_motor_position_control(2.0, -0.0);
+    motor3->set_motor_position_control(1.0, 0.0);
     motor2->set_motor_position_control(1.0, 0.2);
     sleep(2); // sleep 1s
     motor4->disable_motor();
     motor3->disable_motor();
     motor2->disable_motor();
+    motor1->disable_motor();
 }
 
 int main(int argc, char **argv) {
